780dbe76674e0762845f23bbecb39b9eddcd96be
Umair Rafique <umairrafique85@gmail.com>
2018-03-07 16:54:59 +0500

    Last commit lacked the command to show log10 plot. fixed

--- Exploring the Bitcoin cryptocurrency market/testfunctions.R
+++ Exploring the Bitcoin cryptocurrency market/testfunctions.R
@@ -6,17 +6,23 @@ topvolatiles <- rbind(top10Losers, top10Winners)
 
 ggplot(data = topvolatiles, aes(x = reorder(id, -percent_change_24h), y=percent_change_24h, fill=id)) + geom_bar(stat = 'identity')
 
-plotvolatiles <- function(series) {
+plotvolatiles <- function(series, header) {
+  seriesmap <- list(input=c('day', 'week'), mapto=c('percent_change_24h', 'percent_change_7d'))
+  series <- seriesmap$mapto[seriesmap$input==series]
   t7data <- dec6 %>% select(id, percent_change_24h, percent_change_7d)
   top10winners <- head(t7data[order(t7data[,series], decreasing = T),], 10)
   top10winners <- top10winners %>% mutate(status = 'winner')
   top10losers <- head(t7data[order(t7data[,series]),], 10)
   top10losers <- top10losers %>% mutate(status = 'loser')
   topvolatiles <- rbind(top10winners, top10losers)
-  ggplot(data = topvolatiles, aes(x = reorder(id, -series), 
-                                  y=series, fill=id)) + 
+  ggplot(data = topvolatiles, aes(x = reorder(id, -get(series)), 
+                                  y=get(series), fill=id)) + 
     geom_bar(stat = 'identity') + 
-    labs(title = 'Top 10 winners and losers in 24 hrs', y = 'change in %', x = 'coin')
+    labs(title = header, y = 'change in %', x = 'coin') + 
+    theme(plot.title = element_text(hjust=0.5), axis.text.x = element_text(angle=90))
 }
 
-# checkfunc <- 
+capcount <- function(threshhold) {
+  nrow(subset(cap, eval(parse(text=ifelse(grepl(' ', threshhold), paste('market_cap_usd', strsplit(threshhold, ' ')[[1]][1], '& market_cap_usd', strsplit(threshhold, ' ')[[1]][2]), paste('market_cap_usd', threshhold))))))
+}
+
